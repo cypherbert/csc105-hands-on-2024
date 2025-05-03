c7df053f9f18f8fa9a30693240e01d654082a68f
Albert <albert.zaws@mail.kmutt.ac.th>
2025-05-03 23:37:47 +0700

    Adding getToDo route

--- intro-database/src/routes/todo.route.ts
+++ intro-database/src/routes/todo.route.ts
@@ -3,5 +3,6 @@ import * as todoController from "../controllers/todo.controller.ts";
 
 const todoRouter = new Hono();
 todoRouter.post("/", todoController.createTodo);
+todoRouter.get("/", todoController.getTodo);
 
 export { todoRouter };
